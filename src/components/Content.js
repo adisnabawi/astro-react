e33e45fa7ea3de46410858284267692bfbe4934f
adis <adisazizan@gmail.com>
2021-05-29 16:22:36 +0800

    schedule on tv

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,6 +7,7 @@ const Content = (props) => {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
+    const [schedule, setSchedule] = useState([]);
 
 
     useEffect(() => {
@@ -16,6 +17,7 @@ const Content = (props) => {
             (result) => {
             setIsLoaded(true);
             setItems(result.response);
+            setSchedule(result.response.schedule);
             },
             (errors) => {
             setIsLoaded(true);
@@ -28,11 +30,53 @@ const Content = (props) => {
       } else if (!isLoaded) {
         return <div>Loading...</div>
       } else {
-    return (<div className="jumbotron">
+    return (<div>
+        <div className="jumbotron">
         <img src={items.imageUrl} alt="logo" />
         <h1 className="display-4">{items.title}</h1>
         <p>{items.description}</p>
-        </div>)
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="d-flex align-items-start">
+              <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+
+                {
+                  Object.keys(schedule).map((datesc, i) => 
+                    (
+                      <button key={datesc} className={"nav-link" + (i === 0 ? ' active': '')} id={"v-pills-home-" + datesc} data-bs-toggle="pill" data-bs-target={"#v-pills-" + datesc} type="button" role="tab" aria-controls={"v-pills-" + datesc} aria-selected={i === 0 ? ' true': 'false'}>{datesc}</button>
+                    )
+                    
+                   
+                  )
+                }
+                  
+            
+              </div>
+              <div className="tab-content" id="v-pills-tabContent">
+              {
+                  Object.keys(schedule).map((datesc, i) => 
+                    (
+                    <div key={"content-" + i} className={"tab-pane fade show" + (i === 0 ? ' active': '')} id={"v-pills-" + datesc} role="tabpanel" aria-labelledby={"v-pills-" + datesc}>
+                      <ul className="ulsc">
+                        {
+                          schedule[datesc].map(val => (
+                            <li key={val.eventId}>{val.title} - {val.datetime}</li>
+                          )
+
+                        )
+                        }
+                      </ul>
+                      
+                    </div>
+                    )
+                  )
+              }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>)
       }
 }
 
