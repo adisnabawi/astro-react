422c4d6ee96036382850f605f3565b8e01b1f453
adis <adisazizan@gmail.com>
2021-05-31 03:40:18 +0800

    cchecking couple of things

--- src/components/List.js
+++ src/components/List.js
@@ -8,18 +8,19 @@ function refreshPage(id) {
 
 const List = (props) => {
     if(props.find === null || props.find === ''){
-        return (<div className="row">  
+        return (<div className="row" key="rowsearchempty">  
             {props.lists.map(item => (
-                <Router key={"route" + item.id}>
+                <Router key={"route" + item.id + item.stbNumber}>
                         <div className="col-md-3 padbottom nolinkstyle" key={item.id}>
                         <NavLink to={"/content/" + item.id} onClick={()=> refreshPage(item.id)}>
                             <div className="card">
-                            <img className="card-img-top blackbg" src={item.originalImage} alt={item.title} />
+                            <img className="card-img-top blackbg" src={item.originalImage !== null ? item.originalImage:item.imageUrl} alt={item.title} />
                                 <div className="card-body">
-                                    <h4>{item.title}</h4>CH {item.stbNumber}
-                                    <ul className="ulsc">
+                                <h4>{ item.isAstroGoExclusive === false ? item.title : "Astro GO Exclusive Channels" } </h4>
+                                { item.isAstroGoExclusive === false ? "CH " + item.stbNumber : item.title } 
+                                   <ul className="ulsc">
                                     {item.currentSchedule.map(sche => (
-                                        <li>
+                                        <li key={"schedule-eventid-" + sche.eventId}>
                                             <span className="schedule1">{ format(new Date(sche.datetime), 'hh:mm a')}</span> 
                                             <span className="schedule2">{sche.title}</span> 
                                         </li>
@@ -43,16 +44,17 @@ const List = (props) => {
             a++;
             return a;
         })
-        return (<div className="row"> 
+        return (<div className="row" key="rowsearch"> 
         <p>Search on  <b>{props.find}</b>. Total {a} found. </p> 
         {props.lists.filter(itemlis => (itemlis.title.toUpperCase()).includes(props.find.toUpperCase())).map((item,i) => (
                 <Router key={"route" + item.id}>
                         <div className="col-md-3 padbottom nolinkstyle" key={item.id}>
                         <NavLink to={"/content/" + item.id} onClick={()=>refreshPage(item.id)}>
                             <div className="card">
-                                <img className="card-img-top blackbg" src={item.originalImage} alt={item.title} />
+                                <img className="card-img-top blackbg" src={item.originalImage !== null ? item.originalImage:item.imageUrl} alt={item.title} />
                                 <div className="card-body">
-                                <h4>{item.title}</h4>CH {item.stbNumber}
+                                <h4>{ item.isAstroGoExclusive === false ? item.title : "Astro GO Exclusive Channels" } </h4>
+                                { item.isAstroGoExclusive === false ? "CH " + item.stbNumber : item.title } 
                                     <ul className="ulsc">
                                     {item.currentSchedule.map(sche => (
                                         <li>
--- src/components/Search.js
+++ src/components/Search.js
@@ -13,7 +13,7 @@ const Search = (props) => {
             <div className="input-group mb-3">
                 <input type="text" className="form-control" placeholder="Search Channels"  name="search" ref={search}  value={props.finder} />
                 <div className="input-group-append">
-                    <span className="input-group-text" id="basic-addon2" onClick={handleSubmit}>Search</span>
+                    <span className="input-group-text btnsearch" id="basic-addon2" onClick={handleSubmit}><svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path d="M15.7 14.3l-3.1-3.1c2.3-3.1 1.7-7.5-1.4-9.9S3.7-.4 1.3 2.7s-1.7 7.5 1.4 9.9c2.5 1.9 5.9 1.9 8.5 0l3.1 3.1c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4zM2 7c0-2.8 2.2-5 5-5s5 2.2 5 5-2.2 5-5 5c-2.7 0-5-2.2-5-5z"></path></svg></span>
                 </div>
             </div>
         </form>
